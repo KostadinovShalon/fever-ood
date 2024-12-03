4c7242fe34e667d6d2f46d3ba73166ba370676e2
Brian Isaac <kostadinov.shalon@gmail.com>
2024-12-03 12:29:24 +0000

    Classification models and utils init

---
+++ classification/models/wrn_virtual.py
@@ -0,0 +1,174 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models import ResNet
+from torchvision.models.resnet import Bottleneck
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_planes, out_planes, stride, drop_rate=0.0):
+        super(BasicBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.drop_rate = drop_rate
+        self.equalInOut = (in_planes == out_planes)
+        self.convShortcut = (not self.equalInOut) and nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride,
+                                                                padding=0, bias=False) or None
+
+    def forward(self, x):
+        if self.equalInOut:
+            out = self.relu1(self.bn1(x))
+            out = self.relu2(self.bn2(self.conv1(out)))
+        else:
+            x = self.relu1(self.bn1(x))
+            out = self.relu2(self.bn2(self.conv1(x)))
+        if self.drop_rate > 0:
+            out = F.dropout(out, p=self.drop_rate, training=self.training)
+        out = self.conv2(out)
+        if not self.equalInOut:
+            return torch.add(self.convShortcut(x), out)
+        else:
+            return torch.add(x, out)
+
+
+class NetworkBlock(nn.Module):
+    def __init__(self, nb_layers, in_planes, out_planes, block, stride, drop_rate=0.0):
+        super(NetworkBlock, self).__init__()
+        self.layer = self._make_layer(block, in_planes, out_planes, nb_layers, stride, drop_rate)
+
+    def _make_layer(self, block, in_planes, out_planes, nb_layers, stride, drop_rate):
+        layers = []
+        for i in range(nb_layers):
+            layers.append(block(i == 0 and in_planes or out_planes, out_planes, i == 0 and stride or 1, drop_rate))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layer(x)
+
+
+class WideResNet(nn.Module):
+    def __init__(self, depth, num_classes, widen_factor=1, drop_rate=0.0, null_space_red_dim=-1):
+        super(WideResNet, self).__init__()
+        nChannels = [16, 16 * widen_factor, 32 * widen_factor, 64 * widen_factor]
+        assert ((depth - 4) % 6 == 0)
+        n = (depth - 4) // 6
+        block = BasicBlock
+        # 1st conv before any network block
+        self.conv1 = nn.Conv2d(3, nChannels[0], kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        # 1st block
+        self.block1 = NetworkBlock(n, nChannels[0], nChannels[1], block, 1, drop_rate)
+        # 2nd block
+        self.block2 = NetworkBlock(n, nChannels[1], nChannels[2], block, 2, drop_rate)
+        # 3rd block
+        self.block3 = NetworkBlock(n, nChannels[2], nChannels[3], block, 2, drop_rate)
+        # global average pooling and classifier
+        self.bn1 = nn.BatchNorm2d(nChannels[3])
+        self.relu = nn.ReLU(inplace=True)
+        # Assert xor of v2, v3
+        self.null_space_red_dim = null_space_red_dim
+        if null_space_red_dim <= 0:
+            self.fc = nn.Linear(nChannels[3], num_classes)
+        else:
+            self.fc = nn.Sequential(
+                nn.Linear(nChannels[3], null_space_red_dim),
+                nn.ReLU(inplace=True),
+                nn.Linear(null_space_red_dim, num_classes))
+            self.fc[0].bias.data.zero_()
+            self.fc[2].bias.data.zero_()
+        self.nChannels = nChannels[3]
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.block1(out)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.relu(self.bn1(out))
+        out = F.avg_pool2d(out, 8)
+        out = out.view(-1, self.nChannels)
+        return self.fc(out)
+
+    def forward_virtual(self, x):
+        out = self.conv1(x)
+        out = self.block1(out)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.relu(self.bn1(out))
+        out = F.avg_pool2d(out, 8)
+        out = out.view(-1, self.nChannels)
+        if self.null_space_red_dim > 0:
+            out = self.fc[0](out)
+            out = self.fc[1](out)
+            return self.fc[2](out), out
+        return self.fc(out), out
+
+    def intermediate_forward(self, x, layer_index):
+        out = self.conv1(x)
+        out = self.block1(out)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.relu(self.bn1(out))
+        return out
+    
+    def feature_list(self, x):
+        out_list = [] 
+        out = self.conv1(x)
+        out = self.block1(out)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.relu(self.bn1(out))
+        out_list.append(out)
+        out = F.avg_pool2d(out, 8)
+        out = out.view(-1, self.nChannels)
+        return self.fc(out), out_list
+
+
+class VirtualResNet50(ResNet):
+    def __init__(self, num_classes, null_space_red_dim=-1):
+        super().__init__(block=Bottleneck, layers=[3, 4, 6, 3], num_classes=num_classes)
+        self.null_space_red_dim = null_space_red_dim
+        if null_space_red_dim > 0:
+            self.fc = nn.Sequential(
+                nn.Linear(512 * Bottleneck.expansion, null_space_red_dim),
+                nn.ReLU(inplace=True),
+                nn.Linear(null_space_red_dim, num_classes))
+        self.nChannels = 512 * Bottleneck.expansion
+
+    def forward_virtual(self, x):
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        # x = self.fc(x)
+        if self.null_space_red_dim > 0:
+            x = self.fc[0](x)
+            x = self.fc[1](x)
+            return self.fc[2](x), x
+
+        return self.fc(x), x
